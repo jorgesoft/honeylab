2f7a98d94f7c87beff09e5e700e4f5c8bc538d33
Jorge Silva <jorgecarrilloit@gmail.com>
2024-07-17 22:40:42 -0400

    Fixed JSON file creating error

--- fetch_alerts.sh
+++ fetch_alerts.sh
@@ -9,12 +9,15 @@ fi
 # Parameters
 USERNAME=$1
 PASSWORD=$2
-OUTPUT_FILE="alerts.json"
+OUTPUT_FILE="/tmp/alerts.json"
 SIZE=1000  # Number of results per page
 FROM=0
 
 # Start with an empty output file
 > $OUTPUT_FILE
+
+# Open JSON array
+echo "[" > $OUTPUT_FILE
 
 # Fetch total count of alerts for the last 7 days
 TOTAL=$(curl -s -u $USERNAME:$PASSWORD -k -X GET "https://127.0.0.1:9200/wazuh-alerts*/_search" -H 'Content-Type: application/json' -d'
@@ -63,10 +66,21 @@ while [ $FROM -lt $TOTAL ]; do
     "from": '$FROM'
   }')
 
-  # Append the current batch of results to the output file
-  echo $RESPONSE | jq '.hits.hits' >> $OUTPUT_FILE
+  # Process the current batch of results
+  HITS=$(echo $RESPONSE | jq '.hits.hits')
+
+  # Add a comma separator if it's not the first batch
+  if [ $FROM -ne 0 ]; then
+    echo "," >> $OUTPUT_FILE
+  fi
+
+  # Append the current batch of results to the output file, removing the surrounding brackets
+  echo $HITS | sed 's/^\[\(.*\)\]$/\1/' >> $OUTPUT_FILE
 
   FROM=$((FROM + SIZE))
 done
+
+# Close JSON array
+echo "]" >> $OUTPUT_FILE
 
 echo "Results saved to $OUTPUT_FILE"
